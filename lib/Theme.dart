79d67511e819d71d1cca42d8fc858f4d151bae28
Apryn <apriyanriyan23@gmail.com>
2021-12-25 16:56:28 +0700

    Fix Images

--- lib/Theme.dart
+++ lib/Theme.dart
@@ -3,6 +3,7 @@ import 'package:flutter/cupertino.dart';
 Color Orange = Color(0xffFBA651);
 Color Black = Color(0xff191410);
 Color grey = Color(0xffDCDAD9);
+Color Red = Color(0xffED0000);
 
 TextStyle Semibold =
     TextStyle(fontFamily: "Montserrat", fontWeight: FontWeight.w600);
--- lib/home_page.dart
+++ lib/home_page.dart
@@ -56,7 +56,7 @@ class home_page extends StatelessWidget {
               ),
             ),
             Padding(
-              padding: EdgeInsets.symmetric(horizontal: 17, vertical: 23),
+              padding: EdgeInsets.symmetric(horizontal: 17, vertical: 17),
               child: Row(
                 mainAxisAlignment: MainAxisAlignment.spaceBetween,
                 children: [
@@ -113,20 +113,76 @@ class home_page extends StatelessWidget {
                 ],
               ),
             ),
-            Row(
-              children: [
-                Container(
-                  width: 300,
-                  height: 300,
-                  decoration: BoxDecoration(
-                      image: DecorationImage(
+            Padding(
+              padding: const EdgeInsets.symmetric(vertical: 17, horizontal: 17),
+              child: Row(
+                children: [
+                  Container(
+                    width: 300,
+                    height: 300,
+                    decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(25),
+                        image: DecorationImage(
                           image: AssetImage('Assets/images/House-Pic.png'),
-                          fit: BoxFit.cover)),
-                  child: Stack(
-                    children: [Container(color: Colors.black.withOpacity(0.3))],
-                  ),
-                )
-              ],
+                          fit: BoxFit.cover,
+                        )),
+                    child: Stack(
+                      children: [
+                        Container(
+                          decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(25),
+                            color: Colors.black.withOpacity(0.3),
+                          ),
+                        ),
+                        Align(
+                          alignment: Alignment.bottomLeft,
+                          child: Padding(
+                            padding: const EdgeInsets.symmetric(
+                                horizontal: 12, vertical: 11),
+                            child: Row(
+                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                              children: [
+                                Column(
+                                  crossAxisAlignment: CrossAxisAlignment.start,
+                                  children: [
+                                    Text(
+                                      'House Classic',
+                                      style: Bold.copyWith(color: Colors.white),
+                                    ),
+                                    Text(
+                                      'Highway District 201',
+                                      style:
+                                          Regular.copyWith(color: Colors.white),
+                                    ),
+                                  ],
+                                ),
+                                Container(
+                                  alignment: Alignment.center,
+                                  child: IconButton(
+                                    onPressed: () {},
+                                    icon: Icon(
+                                      Icons.favorite,
+                                      color: Red,
+                                      size: 15,
+                                    ),
+                                  ),
+                                  width: 30,
+                                  height: 30,
+                                  decoration: BoxDecoration(
+                                    borderRadius:
+                                        BorderRadiusDirectional.circular(10),
+                                    color: Colors.white,
+                                  ),
+                                )
+                              ],
+                            ),
+                          ),
+                        )
+                      ],
+                    ),
+                  )
+                ],
+              ),
             )
           ],
         ),
